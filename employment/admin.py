f957cfa86bbeb8fa326cfb26aaabca2c0f27d9a1
ppduck11 <1770935132@qq.com>
2023-06-16 11:19:28 +0800

    上传知乎数据

--- employment/admin.py
+++ employment/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from employment.models import Test
+from employment.models import Test,Data
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
@@ -15,3 +15,16 @@ class TestAdmin(ImportExportModelAdmin):
     list_display = ('data_question','data_context')
     search_fields = ('data_question',)  
     resource_class = TestResource
+
+# Register your models here.
+class DataResource(resources.ModelResource):
+
+    class Meta:
+        model = Data
+        export_order = ('date','url','context','voteup','retweet','comment','specialty','origin')
+
+@admin.register(Data)
+class DataAdmin(ImportExportModelAdmin):
+    list_display = ('date','url','context','voteup','retweet','comment','specialty','origin')
+    search_fields = ('date','context','specialty','origin') 
+    resource_class = DataResource
--- employment/models.py
+++ employment/models.py
@@ -7,3 +7,16 @@ class Test(models.Model):
     data_context = models.TextField()
     def __str__(self):
         return self.data_question
+    
+
+class Data(models.Model):
+    date = models.TextField()       # 时间
+    url = models.TextField()        # 网站
+    context = models.TextField()    # 内容
+    voteup = models.TextField()     # 点赞
+    retweet = models.TextField()    # 转发
+    comment = models.TextField()    # 评论
+    specialty = models.TextField()  # 专业
+    origin = models.TextField()     # 来源
+    def __str__(self):
+        return self.context
